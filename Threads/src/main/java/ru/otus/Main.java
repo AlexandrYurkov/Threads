aedd706ffde22c4babdcbd7fc9b901cec6036ef6
aleksandryurkov <Aleksandr.Yurkov@infotecs.ru>
2024-11-02 16:19:20 +0300

    dz

--- Threads/src/main/java/ru/otus/Main.java
+++ Threads/src/main/java/ru/otus/Main.java
@@ -1,7 +1,21 @@
 package ru.otus;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Service service = new Service(25);
+        for(int i = 0; i < 100; i++) {
+            int finalI = i + 1 ;
+            service.execute(() ->{
+                System.out.println("Thread :" + finalI + " -> run!)");
+            });
+            service.shutdown();
+        }
+
+        service.start();
+
     }
 }
